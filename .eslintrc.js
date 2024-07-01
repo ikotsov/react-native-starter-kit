38dbf869561f5226277fa33fbc16f11d95658fa9
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-01 14:01:53 +0300

    chore: customize lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,10 @@
 module.exports = {
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
+  rules: {
+    // Ensures props and state inside functions are always up-to-date
+    'react-hooks/exhaustive-deps': 'warn',
+    // Ensures 'any' will be used only as a last resort
+    '@typescript-eslint/no-explicit-any': 'error'
+  },
 };
