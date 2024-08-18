1a0862809ad3d1ebdd81eec1d0f9d1fc2fe52854
gkotsovilis <i.kotsovilis@gmail.com>
2024-08-18 23:35:58 +0300

    chore: add lint rule for shorthand methods

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     // Ensures props and state inside functions are always up-to-date
     'react-hooks/exhaustive-deps': 'warn',
     // Ensures 'any' will be used only as a last resort
-    '@typescript-eslint/no-explicit-any': 'error'
+    '@typescript-eslint/no-explicit-any': 'error',
+    // Using method shorthand syntax should be avoided, because it can lead to runtime errors.
+    '@typescript-eslint/method-signature-style': ['error', 'property']
   },
   ignorePatterns: ['node_modules/', 'android/', 'ios/']
 };
